ed883ceece2566da576acb78a088975bc954d2a3
DaisukeTomida <61763932+DaisukeTomida@users.noreply.github.com>
2022-01-16 13:07:48 +0900

    ステータス完了時にＬＩＮＥでＢｏｔで利用できない事と、別途担当連絡先をリプライするように修正
    ステータス完了時にＬＩＮＥでＢｏｔで利用できない事と、別途担当連絡先をリプライするように修正

--- line/Linebot.php
+++ line/Linebot.php
@@ -5,6 +5,7 @@ require_once("./logger.php");
 
 $user_id = "";
 $user_name = "";
+$company_code = "";
 $message_config = [
     "平熱"  => "熱はありません",
     "微熱"  => "多少熱がある程度です",
@@ -52,8 +53,10 @@ $log->info('MESSAGE:DB接続[END]');
 $log->info('MESSAGE:ユーザー情報取得[START]');
 try{
     $SQL  = "SELECT ";
-    $SQL .= "   USER_ID, ";
-    $SQL .= "   USER_NAME ";
+    $SQL .= "    USER_ID ";
+    $SQL .= "   ,USER_NAME ";
+    $SQL .= "   ,USER_STATUS ";
+    $SQL .= "   ,COMPANY_CODE ";
     $SQL .= "FROM ";
     $SQL .= "   " . DBPRE . "USER ";
     $SQL .= "WHERE ";
@@ -73,11 +76,55 @@ if($stmt->rowCount()>=0){
     $row = $stmt->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT);
     $user_id = $row[0];
     $user_name = $row[1];
+    $user_status = $row[2];
+    $company_code = $row[3];
     $log->info('user_id:[' . $user_id . '] user_name[' . $user_name . ']');
 }
 $log->info('MESSAGE:データ取得[END]');
 
-if ($user_id==""){
+//会社コードが存在している場合は情報を取得する
+if ($company_code != ''){
+    $log->info('MESSAGE:会社情報取得[START]');
+    try{
+        $SQL  = "SELECT ";
+        $SQL .= "    COMPANY_NAME ";
+        $SQL .= "   ,COMPANY_POST ";
+        $SQL .= "   ,COMPANY_KEN ";
+        $SQL .= "   ,COMPANY_SHI ";
+        $SQL .= "   ,COMPANY_ADDRESS1 ";
+        $SQL .= "   ,COMPANY_ADDRESS2 ";
+        $SQL .= "   ,COMPANY_TEL ";
+        $SQL .= "   ,COMPANY_WEB ";
+        $SQL .= "FROM ";
+        $SQL .= "   " . DBPRE . "M_COMPANY ";
+        $SQL .= "WHERE ";
+        $SQL .= "   COMPANY_CODE		= :company_code ";
+    
+        $log->debug($SQL);
+        $stmt = $dbh->prepare($SQL, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+        $stmt->bindParam(':company_code', $company_code, PDO::PARAM_STR);
+        $stmt->execute();
+        $log->info('MESSAGE:データ取得[START]');
+        if($stmt->rowCount()>=0){
+            $row = $stmt->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT);
+            $company_name = $row[0];
+            $company_post = $row[1];
+            $company_ken = $row[2];
+            $company_shi = $row[3];
+            $company_address1 = $row[4];
+            $company_address2 = $row[5];
+            $company_tel = $row[6];
+            $company_web = $row[7];
+            $log->info('company_name:[' . $company_name . ']');
+        }
+    }catch(PDOException $ex){
+        $log->error("会社情報取得:" . $ex->getMessage());
+        exit;
+    }
+    $log->info('MESSAGE:会社情報取得[END]');
+}
+
+if ($user_id==''){
     //ユーザー登録未完了
     $response_format_text = [
         [
@@ -87,92 +134,126 @@ if ($user_id==""){
     ];
 } else {
     //ユーザー登録完了
-
-    if ((strpos($message_text,'体温報告')) !== false) { 
-        //体温測定
+    if($user_status =='CLEAR'){
+        //完了している場合
+        $return_message_text  = "[住所] 〒" . $company_post . "\n";
+        $return_message_text .= $company_ken . $company_shi . $company_address1 . $company_address2 . "\n";
+        $return_message_text .= "[連絡先] " . $company_tel . "\n";
+        if($company_web != ''){
+            $return_message_text .= "[ホームーページ]\n";
+            $return_message_text .= $company_web . "\n";
+        }
+        $url = "https://www.google.com/maps?q=" . $company_ken . $company_shi . $company_address1 . $company_address2;
+        // レスポンスフォーマット
         $response_format_text = [
             [
-                "type" => "template",
-                "altText" => "現在の体温を教えてください",
-                "template" =>  [
-                    "type" => "buttons",
-                    "title" => "体温調査",
-                    "text" => "現在の体温を教えてください",
-                    "actions" => [
-                            [
-                                "type" => "message",
-                                "label" => "平熱",
-                                "text" => "平熱"
-                            ],
-                            [
-                                "type" => "message",
-                                "label" => "微熱",
-                                "text" => "微熱"
-                            ],
-                            [
-                                "type" => "message",
-                                "label" => "高熱",
-                                "text" => "高熱"
-                            ],
-                        ]
-                ]
-            ]
-        ];
-    } elseif ((strpos($message_text,'体調報告')) !== false) { 
-        //体調返答
-        $response_format_text = [
+                "type" => "text",
+                "text" => "このチャンネルでの連絡は終了しています。\n別途ご連絡ください"
+            ],
             [
                 "type" => "template",
-                "altText" => "現在の体調を教えてください",
+                "altText" => "連絡先情報",
                 "template" =>  [
                     "type" => "buttons",
-                    "title" => "体調調査",
-                    "text" => "現在の体調を教えてください",
+                    "title" => $company_name,
+                    "text" => $return_message_text,
                     "actions" => [
                             [
-                                "type" => "message",
-                                "label" => "無症状",
-                                "text" => "OK"
-                            ],
-                            [
-                                "type" => "message",
-                                "label" => "熱があるまたは息苦しい",
-                                "text" => "NG"
-                            ],
-                            [
-                                "type" => "message",
-                                "label" => "熱があり息苦しい",
-                                "text" => "HELP"
-                            ],
+                                "type" => "uri",
+                                "label" => "位置情報",
+                                "uri" => $url
+                            ]
                         ]
                 ]
             ]
         ];
     }else{
-        switch($message_text){
-            case "平熱":
-            case "微熱":
-            case "高熱":
-                //体温調査
-            case "OK":
-            case "NG":
-            case "HELP":
-                //体調報告
-                if(update_status($dbh, $log, $user_id, $message_config[$message_text])==false){
-                    exit;
-                }
-                $return_message_text = "連絡しました";
-                break;
-            default: 
-                $return_message_text = "申し訳ありません。\r\n「" . $message_text . "」については返答しかねます";
-        };
-        // レスポンスフォーマット
-        $response_format_text = [
-            [
-                "type" => "text",
-                "text" => $return_message_text
-            ]
-        ];
+        if ((strpos($message_text,'体温報告')) !== false) { 
+            //体温測定
+            $response_format_text = [
+                [
+                    "type" => "template",
+                    "altText" => "現在の体温を教えてください",
+                    "template" =>  [
+                        "type" => "buttons",
+                        "title" => "体温調査",
+                        "text" => "現在の体温を教えてください",
+                        "actions" => [
+                                [
+                                    "type" => "message",
+                                    "label" => "平熱",
+                                    "text" => "平熱"
+                                ],
+                                [
+                                    "type" => "message",
+                                    "label" => "微熱",
+                                    "text" => "微熱"
+                                ],
+                                [
+                                    "type" => "message",
+                                    "label" => "高熱",
+                                    "text" => "高熱"
+                                ],
+                            ]
+                    ]
+                ]
+            ];
+        } elseif ((strpos($message_text,'体調報告')) !== false) { 
+            //体調返答
+            $response_format_text = [
+                [
+                    "type" => "template",
+                    "altText" => "現在の体調を教えてください",
+                    "template" =>  [
+                        "type" => "buttons",
+                        "title" => "体調調査",
+                        "text" => "現在の体調を教えてください",
+                        "actions" => [
+                                [
+                                    "type" => "message",
+                                    "label" => "無症状",
+                                    "text" => "OK"
+                                ],
+                                [
+                                    "type" => "message",
+                                    "label" => "熱があるまたは息苦しい",
+                                    "text" => "NG"
+                                ],
+                                [
+                                    "type" => "message",
+                                    "label" => "熱があり息苦しい",
+                                    "text" => "HELP"
+                                ],
+                            ]
+                    ]
+                ]
+            ];
+        }else{
+            switch($message_text){
+                case "平熱":
+                case "微熱":
+                case "高熱":
+                    //体温調査
+                case "OK":
+                case "NG":
+                case "HELP":
+                    //体調報告
+                    if(update_status($dbh, $log, $user_id, $message_config[$message_text])==false){
+                        exit;
+                    }
+                    $return_message_text = "連絡しました";
+                    break;
+                default: 
+                    $return_message_text = "申し訳ありません。\r\n「" . $message_text . "」については返答しかねます";
+            };
+            // レスポンスフォーマット
+            $response_format_text = [
+                [
+                    "type" => "text",
+                    "text" => $return_message_text
+                ]
+            ];
+        }
     }
 }
 //ポストデータ
